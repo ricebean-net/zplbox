b4c71c02369ffb398baab4b06dda95db9606f75a
Stefan Meissner <stefan.meissner@vista.com>
2024-08-18 13:42:10 +0200

    api documentation and improvements

--- src/main/java/com/meixxi/html2zpl/controller/v1/Html2ZplController.java
+++ src/main/java/com/meixxi/html2zpl/controller/v1/Html2ZplController.java
@@ -1,13 +1,16 @@
 package com.meixxi.html2zpl.controller.v1;
 
+import com.meixxi.html2zpl.controller.util.Responses;
 import com.meixxi.html2zpl.model.RenderingParams;
 import com.meixxi.html2zpl.service.image.ImageService;
 import com.meixxi.html2zpl.service.render.RenderService;
 import com.meixxi.html2zpl.service.zpl.ZplService;
+import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -35,7 +38,7 @@ public class Html2ZplController {
     private ZplService zplService;
 
     @PostMapping(value = "", consumes = APPLICATION_JSON_VALUE, produces = {IMAGE_PNG_VALUE, APPLICATION_PROBLEM_JSON_VALUE})
-    public ResponseEntity<?> createZplLabel(@RequestBody RenderingParams renderingParams) throws Exception {
+    public ResponseEntity<?> createZplLabel(@RequestBody RenderingParams renderingParams, HttpServletRequest httpServletRequest) throws Exception {
         log.info("New request has been received...");
 
        try {
@@ -50,13 +53,11 @@ public class Html2ZplController {
            String zplData = zplService.createLabel(bufferedImage);
 
            // return zpl data
-           HttpHeaders headers = new HttpHeaders();
-           headers.add(HttpHeaders.CONTENT_TYPE, TEXT_PLAIN_VALUE);
-           return new ResponseEntity<>(zplData, headers, HttpStatus.OK);
+           return Responses.createOkResponse(zplData, TEXT_PLAIN_VALUE);
 
        } catch (Exception ex) {
 
-           return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+           return Responses.createBadRequestResponse(httpServletRequest, ex);
        }
     }
 }
