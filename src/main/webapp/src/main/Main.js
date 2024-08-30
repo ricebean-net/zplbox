0cdc6560ca9a11abe9948ccfd7f7f07e08d0f8cc
Stefan Meissner <stefan.meissner@vista.com>
2024-08-31 00:18:47 +0200

    manual ui: label source

--- src/main/webapp/src/main/Main.js
+++ src/main/webapp/src/main/Main.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { saveAs } from "file-saver"
 import Toast from "./Toast";
+import LabelSource from "./modules/LabelSource";
 
 
 function Main() {
@@ -18,125 +19,141 @@ function Main() {
     }
 
 
-    // label url handling
-    const [urlLabel, setUrlLabel] = useState("");
-
-    const baseUrl = `${window.location.protocol}//${window.location.hostname}${window.location.port ? ':' + window.location.port : ''}`;
-    const urlTestLabels = [
-        { descriptiveName: "UPS Example (4 x 8 inches)", url: baseUrl + "/labels/ups-example.html" },
-        { descriptiveName: "Generic Example (4 x 6 inches)", url: baseUrl + "/xxx.html" }
-    ]
-
-
-    // construct request payload
+    // payload construction
+    const [labelSource, setLabelSource] = useState(undefined);
     const [payload, setPayload] = useState(undefined);
 
     useEffect(() => {
-        setPayload({
-            url: urlLabel,
-            widthPts: 812,
-            heightPts: 1624
-        });
+        if (labelSource === undefined) {
+            setPayload(undefined)
+            return;
+        }
 
-    }, [urlLabel]);
+        const newPayload = { ...payload };
+
+        if (labelSource === undefined) {
+            newPayload.dataBase64 = undefined;
+            newPayload.url = undefined;
+        } else {
+            newPayload.dataBase64 = labelSource.dataBase64;
+            newPayload.url = labelSource.url;
+        }
+
+        setPayload(newPayload);
+    }, [labelSource])
 
 
     // construct curl request
-    const [curlCommand, setCurlCommand] = useState(undefined);
+    const [cmdCurl, setCmdCurl] = useState("");
 
     useEffect(() => {
+        if (payload === undefined) {
+            setCmdCurl(undefined);
+            return;
+        }
+
+
+        const baseUrl = `${window.location.protocol}//${window.location.hostname}${window.location.port ? ':' + window.location.port : ''}`;
 
         const cmdLines = [
             `curl --request POST`,
-            `     --url ${baseUrl}/v1/html2zpl`,
+            `     --url ${baseUrl}/v1/TBD`,
             `     --header 'content-type: application/json'`,
             `     --data '${JSON.stringify(payload)}'`
         ]
 
-
-        setCurlCommand(cmdLines.join(" \\ \n"));
-
+        setCmdCurl(cmdLines.join(" \\ \n"));
     }, [payload])
 
 
-    // zpl label generation
-    const [zplGenerationActive, setZplGenerationActive] = useState(false);
-    const [zplData, setZplData] = useState("")
+    // // label url handling
+    // const [urlLabel, setUrlLabel] = useState("");
 
-    const generateZplLabel = () => {
-
-        setZplGenerationActive(true);
-
-        fetch('/v1/html2zpl', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(payload)
-        })
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Network response was not ok');
-                }
-                return response.text();
-            })
-            .then(zplData => {
-                setZplData(zplData);
-                showEvent("ZPL Generation Successful.", "Your ZPL label has been generated successfully.", "success")
-            })
-            .catch(error => {
-                console.error('Error:', error);
-                showEvent("Error ZPL Generation.", error.message, "danger")
-            })
-            .finally(() => setZplGenerationActive(false));
-    }
+    // const baseUrl = `${window.location.protocol}//${window.location.hostname}${window.location.port ? ':' + window.location.port : ''}`;
+    // const urlTestLabels = [
+    //     { descriptiveName: "UPS Example (4 x 8 inches)", url: baseUrl + "/labels/ups-example.html" },
+    //     { descriptiveName: "Generic Example (4 x 6 inches)", url: baseUrl + "/xxx.html" }
+    // ]
 
 
-    // zpl export 
-    const copyZplToClipboard = () => {
-        navigator.clipboard.writeText(zplData)
-            .then(() => showEvent("Copy to clipboard successful", "Your ZPL label has been copied successfully to the clipboard.", "success"))
-            .catch(() => showEvent("Error copy to clipboard", "Error copying to clipboard.", "danger"));
-    }
+    // // construct request payload
+    // const [payload, setPayload] = useState(undefined);
 
-    const saveZplAsFile = () => {
-        var blob = new Blob([zplData], { type: "text/plain;charset=utf-8" });
-        saveAs(blob, "hello world.zpl.txt");
-    }
+    // useEffect(() => {
+    //     setPayload({
+    //         url: urlLabel,
+    //         widthPts: 812,
+    //         heightPts: 1624
+    //     });
+
+    // }, [urlLabel]);
+
+
+
+
+
+    // // zpl label generation
+    // const [zplGenerationActive, setZplGenerationActive] = useState(false);
+    // const [zplData, setZplData] = useState("")
+
+    // const generateZplLabel = () => {
+
+    //     setZplGenerationActive(true);
+
+    //     fetch('/v1/html2zpl', {
+    //         method: 'POST',
+    //         headers: {
+    //             'Content-Type': 'application/json'
+    //         },
+    //         body: JSON.stringify(payload)
+    //     })
+    //         .then(response => {
+    //             if (!response.ok) {
+    //                 throw new Error('Network response was not ok');
+    //             }
+    //             return response.text();
+    //         })
+    //         .then(zplData => {
+    //             setZplData(zplData);
+    //             showEvent("ZPL Generation Successful.", "Your ZPL label has been generated successfully.", "success")
+    //         })
+    //         .catch(error => {
+    //             console.error('Error:', error);
+    //             showEvent("Error ZPL Generation.", error.message, "danger")
+    //         })
+    //         .finally(() => setZplGenerationActive(false));
+    // }
+
+
+    // // zpl export 
+    // const copyZplToClipboard = () => {
+    //     navigator.clipboard.writeText(zplData)
+    //         .then(() => showEvent("Copy to clipboard successful", "Your ZPL label has been copied successfully to the clipboard.", "success"))
+    //         .catch(() => showEvent("Error copy to clipboard", "Error copying to clipboard.", "danger"));
+    // }
+
+    // const saveZplAsFile = () => {
+    //     var blob = new Blob([zplData], { type: "text/plain;charset=utf-8" });
+    //     saveAs(blob, "hello world.zpl.txt");
+    // }
 
 
     // return final output
     return (
         <div className="container mt-4">
 
-            {/* label url */}
-            <div className="row">
-                <div className="col-2">Label URL (HTML/PDF):</div>
-                <div className="col-4">
-                    <div className="input-group">
-                        <input type="text" className="form-control" placeholder="name@example.com" value={urlLabel} onChange={(e) => setUrlLabel(e.target.value)} />
-                        <button type="button" className="btn btn-outline-secondary dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown" aria-expanded="false">
-                            <span className="visually-hidden">Toggle Dropdown</span>
-                        </button>
-                        <ul className="dropdown-menu dropdown-menu-end w-100">
-                            {urlTestLabels.map((urlTestUrl, idx) =>
-                                <li key={'testLabel-' + idx}>
-                                    <button type="button" className="dropdown-item py-2" onClick={() => setUrlLabel(urlTestUrl.url)}>
-                                        {urlTestUrl.descriptiveName}<br />
-                                        <small>{urlTestUrl.url}</small>
-                                    </button>
-
-                                </li>
-                            )}
-
-                        </ul>
-                    </div>
+            <div className="row mb-2">
+                <div className="col">
+                    <h2>1. Reference or upload your label</h2>
                 </div>
-                <div className="col-6">asfa</div>
             </div>
 
+            <LabelSource onLabelUpdate={setLabelSource} />
+
+
+
             {/* create label */}
-            <div className="row mt-2">
+            {/* <div className="row mt-2">
                 <div className="col-2"></div>
                 <div className="col-4 text-end">
                     <button type="button" className="btn btn-outline-primary" onClick={generateZplLabel} disabled={zplGenerationActive}>
@@ -146,20 +163,22 @@ function Main() {
                         Create ZPL Label</button>
                 </div>
                 <div className="col-6"></div>
-            </div>
+            </div> */}
 
             {/* technical connectiviy */}
             <div className="row mt-5">
                 <div className="col-6">
-                    <pre>{JSON.stringify(payload, null, 2)}</pre>
+                    <small>JSON Payload:</small>
+                    <textarea className="form-control form-control-sm font-monospace bg-body-tertiary" rows={5} value={payload === undefined ? "" : JSON.stringify(payload, null, 2)} readOnly={true} style={{ resize: 'none' }} />
                 </div>
                 <div className="col-6">
-                    <pre>{curlCommand}</pre>
+                    <small>curl command:</small>
+                    <textarea className="form-control form-control-sm font-monospace bg-body-tertiary" rows={5} value={cmdCurl === undefined ? "" : cmdCurl} readOnly={true} style={{ resize: 'none' }} />
                 </div>
             </div>
 
             {/* zpl data */}
-            <div className="row mt-5">
+            {/* <div className="row mt-5">
                 <div className="col-12">
                     <pre>{zplData}</pre>
                 </div>
@@ -170,7 +189,7 @@ function Main() {
                     <button type="button" className="btn btn-outline-secondary btn-sm me-3" onClick={copyZplToClipboard}><i className="bi bi-copy me-2"></i>Copy to Clipboard</button>
                     <button type="button" className="btn btn-outline-secondary btn-sm" onClick={saveZplAsFile}><i className="bi bi-cloud-download me-2"></i>Download</button>
                 </div>
-            </div>
+            </div> */}
 
             {/* toasts */}
             <Toast event={event} onResetEvent={() => setEvent(undefined)} />
