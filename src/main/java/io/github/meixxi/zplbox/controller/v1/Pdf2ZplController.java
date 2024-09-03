5de784d375abb2fadc7fe8d3fbfed8813b45d101
Stefan Meissner <stefan.meissner@vista.com>
2024-09-03 15:05:04 +0200

    enable print service

--- src/main/java/io/github/meixxi/zplbox/controller/v1/Pdf2ZplController.java
+++ src/main/java/io/github/meixxi/zplbox/controller/v1/Pdf2ZplController.java
@@ -3,6 +3,7 @@ package io.github.meixxi.zplbox.controller.v1;
 import io.github.meixxi.zplbox.controller.util.Responses;
 import io.github.meixxi.zplbox.controller.v1.model.PdfRenderingParams;
 import io.github.meixxi.zplbox.service.image.ImageService;
+import io.github.meixxi.zplbox.service.print.PrintService;
 import io.github.meixxi.zplbox.service.render.PdfRenderService;
 import io.github.meixxi.zplbox.service.zpl.ZplService;
 import io.github.meixxi.zplbox.util.Urls;
@@ -11,10 +12,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.awt.image.BufferedImage;
 import java.io.IOException;
@@ -39,6 +37,9 @@ public class Pdf2ZplController {
 
     @Autowired
     private ZplService zplService;
+
+    @Autowired
+    private PrintService printService;
 
     @PostMapping(value = "", consumes = APPLICATION_JSON_VALUE, produces = {TEXT_PLAIN_VALUE, APPLICATION_PROBLEM_JSON_VALUE})
     public ResponseEntity<?> createZplLabel(@RequestBody PdfRenderingParams pdfRenderingParams, HttpServletRequest httpServletRequest) throws IOException {
@@ -71,6 +72,48 @@ public class Pdf2ZplController {
 
             // return zpl data
             return Responses.createOkResponse(zplData, TEXT_PLAIN_VALUE);
+
+        } catch (Exception ex) {
+            return Responses.createBadRequestResponse(httpServletRequest, ex);
+
+        }
+    }
+
+    @PostMapping(value = "/print/{tcpAddress}", consumes = APPLICATION_JSON_VALUE, produces = {APPLICATION_PROBLEM_JSON_VALUE})
+    public ResponseEntity<?> createSendZplLabel(@PathVariable String tcpAddress, @RequestBody PdfRenderingParams pdfRenderingParams, HttpServletRequest httpServletRequest) throws IOException {
+        log.info("New request 'Create ZPL Label' has been received...");
+        URI sourceUri = null;
+
+        try {
+
+            // normalize url
+            sourceUri = Urls.normalize(pdfRenderingParams.getUrl(), pdfRenderingParams.getDataBase64());
+            log.info("Process file '{}'...", sourceUri);
+
+            // render web content
+            BufferedImage bufferedImage = pdfRenderService.render(sourceUri, pdfRenderingParams.getDotsPerInch());
+            log.info("WebContent has been rendered successfully.");
+
+            // clean up
+            if(sourceUri != null && Objects.equals(sourceUri.getScheme(), "file")) {
+                Files.delete(Path.of(sourceUri));
+                log.info("File '{}' has been cleaned.", sourceUri);
+            }
+
+            // convert to monochrome
+            bufferedImage = imageService.convertToMonochrome(bufferedImage);
+            log.info("Rendered image has been converted to monochrome.");
+
+            // create ZPL data
+            String zplData = zplService.createLabel(bufferedImage);
+            log.info("ZPL Data has been created.");
+
+            // forward to tcp
+            URI uri = new URI("tcp://" + tcpAddress);
+            printService.printLabel(zplData, uri.getHost(), uri.getPort());
+
+            // return zpl data
+            return Responses.createOkResponse();
 
         } catch (Exception ex) {
             return Responses.createBadRequestResponse(httpServletRequest, ex);
--- src/main/webapp/src/main/modules/ZplConversionConfig.js
+++ src/main/webapp/src/main/modules/ZplConversionConfig.js
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 
-function ZplConversionConfig({ endpoint, payload, onEndpointUpdate, onConfigUpdate }) {
+function ZplConversionConfig({ endpointBase, payload, onEndpointBaseUpdate, onConfigUpdate }) {
 
     // html2zpl config
     const [widthInches, setWidthInches] = useState(undefined);
@@ -49,40 +49,40 @@ function ZplConversionConfig({ endpoint, payload, onEndpointUpdate, onConfigUpda
     }, [dotsPerInch])
 
 
-    // endpoint configuration
+    // endpoint base configuration
     useEffect(() => {
 
         // reset settings
-        if (endpoint === "html2zpl") {
+        if (endpointBase === "html2zpl") {
             resetHtmlConfig();
-        } else if (endpoint === "pdf2zpl") {
+        } else if (endpointBase === "pdf2zpl") {
             resetPdfConfig();
         }
 
-    }, [endpoint])
+    }, [endpointBase])
 
 
     // return final output
     return (
 
         <div className="row mt-2">
-            <div className="col-2">Endpoint:</div>
+            <div className="col-2">Endpoint Base:</div>
             <div className="col-5 border-end">
 
-                {/* endpoint switch */}
+                {/* endpoint base switch */}
                 <div className="container-fluid p-0 mb-3">
                     <div className="form-check form-check-inline me-4">
-                        <input className="form-check-input" type="radio" checked={endpoint === "html2zpl"} onChange={(e) => onEndpointUpdate("html2zpl")} />
+                        <input className="form-check-input" type="radio" checked={endpointBase === "html2zpl"} onChange={(e) => onEndpointBaseUpdate("html2zpl")} />
                         <label className="form-check-label">/v1/html2zpl <small><i>(Web Content)</i></small></label>
                     </div>
                     <div className="form-check form-check-inline">
-                        <input className="form-check-input" type="radio" checked={endpoint === "pdf2zpl"} onChange={(e) => onEndpointUpdate("pdf2zpl")} />
+                        <input className="form-check-input" type="radio" checked={endpointBase === "pdf2zpl"} onChange={(e) => onEndpointBaseUpdate("pdf2zpl")} />
                         <label className="form-check-label">/v1/html2zpl <small><i>(PDF Document)</i></small></label>
                     </div>
                 </div>
 
                 {/* html2zpl config */}
-                {endpoint === "html2zpl" &&
+                {endpointBase === "html2zpl" &&
                     <div className="container-fluid p-0">
                         <div className="row">
                             <div className="col-3">
@@ -117,7 +117,7 @@ function ZplConversionConfig({ endpoint, payload, onEndpointUpdate, onConfigUpda
                     </div>
                 }
 
-                {endpoint === "pdf2zpl" &&
+                {endpointBase === "pdf2zpl" &&
                     <div className="container-fluid p-0">
                         <div className="row">
                             <div className="col-3">
