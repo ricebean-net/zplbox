76f4fffa5ae847bb5e4b06b7b718b5ba12702df8
Stefan Meissner <stefan.meissner@vista.com>
2024-11-29 09:34:32 +0100

    show rotation in ui

--- src/main/webapp/src/common/Header.js
+++ src/main/webapp/src/common/Header.js
@@ -12,23 +12,23 @@ function Header() {
             </div>
 
             <div>
-                <div class="btn-group pe-4">
-                    <button type="button" class="btn dropdown-toggle py-0" data-bs-toggle="dropdown" aria-expanded="false">
+                <div className="btn-group pe-4">
+                    <button type="button" className="btn dropdown-toggle py-0" data-bs-toggle="dropdown" aria-expanded="false">
                         Documentation
                     </button>
-                    <ul class="dropdown-menu dropdown-menu-end">
-                        <li><a class="dropdown-item py-2" href="https://zplbox.org/#api-reference" target='_blank'>API Reference</a></li>
-                        <li><a class="dropdown-item py-2" href="https://github.com/meiXXI/zplbox" target='_blank'>Technical Documentation</a></li>
+                    <ul className="dropdown-menu dropdown-menu-end">
+                        <li><a className="dropdown-item py-2" href="https://zplbox.org/#api-reference" target='_blank'>API Reference</a></li>
+                        <li><a className="dropdown-item py-2" href="https://github.com/meiXXI/zplbox" target='_blank'>Technical Documentation</a></li>
                     </ul>
                 </div>
-                <div class="btn-group pe-5">
-                    <button type="button" class="btn dropdown-toggle py-0" data-bs-toggle="dropdown" aria-expanded="false">
+                <div className="btn-group pe-5">
+                    <button type="button" className="btn dropdown-toggle py-0" data-bs-toggle="dropdown" aria-expanded="false">
                         About
                     </button>
-                    <ul class="dropdown-menu dropdown-menu-end">
-                        <li><a class="dropdown-item py-2" href="https://zplbox.org/#api-reference" target='_blank'><i class="bi bi-github me-2"></i>ZplBox on GitHub</a></li>
-                        <li><hr class="dropdown-divider" /></li>
-                        <li><a class="dropdown-item" href="https://buymeacoffee.com/meixxi" target='_blank'><img src={bmc} style={{width: 100}} /></a></li>
+                    <ul className="dropdown-menu dropdown-menu-end">
+                        <li><a className="dropdown-item py-2" href="https://zplbox.org/#api-reference" target='_blank'><i className="bi bi-github me-2"></i>ZplBox on GitHub</a></li>
+                        <li><hr className="dropdown-divider" /></li>
+                        <li><a className="dropdown-item" href="https://buymeacoffee.com/meixxi" target='_blank'><img src={bmc} style={{width: 100}} /></a></li>
                     </ul>
                 </div>
             </div>
