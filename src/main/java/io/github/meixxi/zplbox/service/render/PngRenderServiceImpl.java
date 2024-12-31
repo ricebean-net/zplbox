346f075abfc122b46f2d4c097dbcc5e482fd8969
Stefan Meissner <stefan.meissner@vista.com>
2024-12-31 21:27:20 +0100

    migration to debian

--- src/main/java/io/github/meixxi/zplbox/service/render/PngRenderServiceImpl.java
+++ src/main/java/io/github/meixxi/zplbox/service/render/PngRenderServiceImpl.java
@@ -1,12 +1,14 @@
 package io.github.meixxi.zplbox.service.render;
 
 import io.github.meixxi.zplbox.util.URIs;
+import org.springframework.stereotype.Service;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayInputStream;
 import java.net.URI;
 
+@Service
 public class PngRenderServiceImpl implements PngRenderService {
 
     @Override
