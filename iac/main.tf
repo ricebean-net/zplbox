bae613ab63fde6e27a6c81bb06c2471e28388634
Dr. Stefan Meissner <stefan.meissner@vista.com>
2025-09-03 12:55:41 +0200

    next try

--- iac/main.tf
+++ iac/main.tf
@@ -28,13 +28,26 @@ resource "google_project_iam_binding" "service_account_user_binding" {
 }
 
 # This grants the Cloud Run Service Agent permission to read from Artifact Registry
-resource "google_project_iam_binding" "artifact_registry_reader_binding" {
-  project = var.gcp_project_id
-  role    = "roles/artifactregistry.reader"
-  members = [
-    "serviceAccount:service-${data.google_project.project.number}@gcp-sa-run.iam.gserviceaccount.com"
-  ]
-  depends_on = [google_cloud_run_v2_service.default]
+# resource "google_project_iam_binding" "artifact_registry_reader_binding" {
+#   project = var.gcp_project_id
+#   role    = "roles/artifactregistry.reader"
+#   members = [
+#     "serviceAccount:service-${data.google_project.project.number}@gcp-sa-run.iam.gserviceaccount.com"
+#   ]
+#   depends_on = [google_cloud_run_v2_service.default]
+# }
+
+# This creates a secret with your GitLab credentials for Cloud Run to use
+resource "google_cloud_run_v2_service_template_secret" "gitlab_auth" {
+  location = var.gcp_region
+  service  = google_cloud_run_v2_service.default.name
+  name     = "gitlab-auth"
+
+  secret {
+    secret_name = "gitlab-auth"
+    username    = "gitlab-ci-token" # Or your deploy token's username
+    password    = "$CI_JOB_TOKEN"   # Or your deploy token's value
+  }
 }
 
 # The Cloud Run v2 service
@@ -44,7 +57,11 @@ resource "google_cloud_run_v2_service" "default" {
 
   template {
     containers {
-      image = var.gitlab_image_name
+      image = "registry.gitlab.com/ricebean.net/zplbox/zplbox:1.0.4" # The original GitLab image path
+    }
+    # This block tells Cloud Run where to find the auth secret
+    image_pull_secrets {
+      secret_name = google_cloud_run_v2_service_template_secret.gitlab_auth.name
     }
   }
 }
