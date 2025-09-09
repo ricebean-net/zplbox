66e5b7b4a3c1872ed4ec59e5e39dfa977d2adc01
Dr. Stefan Meissner <stefan.meissner@ricebean.net>
2025-09-09 20:30:38 +0200

    update security config

--- src/main/java/net/ricebean/zplbox/config/RapidApiSecurityConfig.java
+++ src/main/java/net/ricebean/zplbox/config/RapidApiSecurityConfig.java
@@ -1,19 +1,15 @@
 package net.ricebean.zplbox.config;
 
-import jakarta.servlet.Filter;
-import jakarta.servlet.FilterChain;
-import jakarta.servlet.ServletRequest;
-import jakarta.servlet.ServletResponse;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
+import net.ricebean.zplbox.config.filter.ApiKeyAuthFilter;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
@@ -23,38 +19,20 @@ public class RapidApiSecurityConfig {
 
     private static final Logger log = LoggerFactory.getLogger(RapidApiSecurityConfig.class);
 
-    private static final String RAPID_API_HEADER = "X-RapidAPI-Proxy-Secret";
-
-    @Value("${rapidapi.secret}")
-    private String rapidapiSecret;
+    @Autowired
+    private ApiKeyAuthFilter apiKeyAuthFilter;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
-        // Define the Filter logic inline
-        Filter customHeaderFilter = (ServletRequest request, ServletResponse response, FilterChain filterChain) -> {
-            HttpServletRequest httpRequest = (HttpServletRequest) request;
-            HttpServletResponse httpResponse = (HttpServletResponse) response;
-
-            String headerValue = httpRequest.getHeader(RAPID_API_HEADER);
-
-            if (headerValue == null || !headerValue.equals(rapidapiSecret)) {
-                httpResponse.setStatus(HttpServletResponse.SC_UNAUTHORIZED);
-                return;
-            }
-
-            filterChain.doFilter(request, response);
-        };
-
         http
                 .csrf(AbstractHttpConfigurer::disable)
-                .addFilterBefore(customHeaderFilter, UsernamePasswordAuthenticationFilter.class)
-                .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/**").permitAll()
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(auth -> auth
+                        .requestMatchers("/version").permitAll()
                         .anyRequest().authenticated()
-                );
+                )
+                .addFilterBefore(apiKeyAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
     }
-
 }
---
+++ src/main/java/net/ricebean/zplbox/config/filter/ApiKeyAuthFilter.java
@@ -0,0 +1,42 @@
+package net.ricebean.zplbox.config.filter;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class ApiKeyAuthFilter extends OncePerRequestFilter {
+
+    private static final String API_SECRET_HEADER = "X-RapidAPI-Proxy-Secret";
+
+    @Value("${rapidapi.secret}")
+    private String rapidapiSecret;
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+
+        String requestSecret = request.getHeader(API_SECRET_HEADER);
+
+        if (rapidapiSecret.equals(requestSecret)) {
+            var authentication = new UsernamePasswordAuthenticationToken(
+                    "api-user", // principal
+                    null,       // credentials
+                    AuthorityUtils.createAuthorityList("ROLE_USER") // authorities
+            );
+
+            SecurityContextHolder.getContext().setAuthentication(authentication);
+        }
+
+        filterChain.doFilter(request, response);
+    }
+}
