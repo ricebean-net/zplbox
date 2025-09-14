f4a07042f12c27fb5b5803388295ebdcb8a948ef
Dr. Stefan Meissner <stefan.meissner@ricebean.net>
2025-09-14 10:42:14 +0200

    gcp: add startup cpu boost

--- iac/main.tf
+++ iac/main.tf
@@ -20,6 +20,7 @@ resource "google_cloud_run_v2_service" "default" {
           cpu    = "2"
           memory = "4Gi"
         }
+        startup_cpu_boost = true
       }
       env {
         name  = "rapidapi.secret"
